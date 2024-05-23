6cb01709e39975937af5051ed5c2f45e9921238e
sonit <sorsonit06@gmail.com>
2024-05-23 23:47:54 +0700

    deleted

--- e3.js
+++
@@ -1,8 +0,0 @@
-// bubble.js
-const bubbleContainer = document.getElementById('bubbleContainer');
-for (let i = 0; i < 10; i++) {
-    const bubble = document.createElement('div');
-    bubble.className = 'bubble';
-    bubble.textContent = '🔵';
-    bubbleContainer.appendChild(bubble);
-}
