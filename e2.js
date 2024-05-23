5383eb928ae33e1d35d18200086365619866e731
sonit <sorsonit06@gmail.com>
2024-05-23 19:09:56 +0700

    rearrange file

--- e2.js
+++
@@ -1,8 +0,0 @@
-// snow.js
-const snowContainer = document.getElementById('snowContainer');
-for (let i = 0; i < 10; i++) {
-    const snowflake = document.createElement('div');
-    snowflake.className = 'snowflake';
-    snowflake.textContent = '❄';
-    snowContainer.appendChild(snowflake);
-}
